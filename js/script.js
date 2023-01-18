cc079d1450f056ebb897abb6747f844e35e7482b
aautoruns <galihazella06@gmail.com>
2023-01-18 11:40:25 +0700

    commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// Toggle class active
+const navbarNav = document.querySelector('.navbar-nav');
+
+// Ketika hamburger menu diklik
+document.querySelector('#hamburger-menu').onclick = () => {
+  navbarNav.classList.toggle('active');
+};
+
+// Klik diluar sidebar menu untuk menghilangkan navbar
+const hamburger = document.querySelector('#hamburger-menu');
+document.addEventListener('click', function (e) {
+  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove('active');
+  }
+});
